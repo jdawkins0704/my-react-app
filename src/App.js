10e1be813fbdecdfb58bf1af2745182b2ccd12ec
Jared Dawkins <jdawkins0704@gmail.com>
2025-11-08 23:44:00 -0600

    implemented new changes regarding for when button is clicked

--- src/App.js
+++ src/App.js
@@ -3,18 +3,9 @@ import { useState } from 'react';
 function Square() {
   const [value, setValue] = useState(null);
 
-     function handleClick() {
-setValue('X');
+  function handleClick() {
+    setValue('X');
   }
-
-  return (
-    <button
-      className="square"
-      onClick={handleClick}
-    >
-      {value}
-    </button>
-  );
 
   return (
     <button
@@ -29,7 +20,7 @@ setValue('X');
 export default function Board() {
   return (
     <>
-     <div className="board-row">
+      <div className="board-row">
         <Square />
         <Square />
         <Square />
