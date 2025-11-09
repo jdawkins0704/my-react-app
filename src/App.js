f5cccd2f79ed3cf8db2171c678621fe05465f4d6
Jared Dawkins <jdawkins0704@gmail.com>
2025-11-09 00:18:08 -0600

    added part for game component to keep track of which step the user is currently viewing

--- src/App.js
+++ src/App.js
@@ -55,6 +55,7 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [currentMove, setCurrentMove] = useState(0);
   const currentSquares = history[history.length - 1];
 
   function handlePlay(nextSquares) {
@@ -74,7 +75,7 @@ export default function Game() {
       description = 'Go to game start';
     }
     return (
-    <li key={move}>
+      <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
     );
