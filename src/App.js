77462f35525f28360ab1a53c5bbace6dd07ac516
Jared Dawkins <jdawkins0704@gmail.com>
2025-11-09 00:04:29 -0600

    updated handleClick to see if a player has won

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-    if (squares[i]) {
-      return;
-    }
+   if (squares[i] || calculateWinner(squares)) {
+    return;
+  }
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = 'X';
